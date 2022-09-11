898d69e8db9aea4fade2403633b8e36a5bca6089
atilaahmettaner <67838093+atilaahmettaner@users.noreply.github.com>
2022-09-11 22:44:17 +0300

    add coin image

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template, request
 from tradingview_ta import *
+import pywhatkit
+import pymongo
+from pymongo import MongoClient
 with open('KUCOIN_BINANCE_HUOBI.txt') as f:
     lines = f.read()
     line = lines.split('\n')
+
+
 app = Flask(__name__)
+class crypto:
+ def __init__(self,coinName,BBW_value,rsi_value):
+    self.coinName =coinName
+    self.BBW_value=BBW_value
+    self.rsi_value=rsi_value
 line_list =[]
+line_list1 =[]
+data={}
 @app.route('/', methods=['GET', 'POST'])
 def hoursStore():
     return render_template('index.html', Hourss=["15m", "1h", "4h", "1D", "1W", "1M"])
@@ -18,9 +30,7 @@ def Scan():
     analysis = get_multiple_analysis(screener="crypto", interval=a, symbols=line)
     for key, value  in analysis.items():
         try:
-
             if value != None:
-
                 open = value.indicators["open"]
                 close = value.indicators["close"]
                 macd = value.indicators["MACD.macd"]
@@ -44,12 +54,17 @@ def Scan():
                         dir = {
                             exchange: coin,
                         }
+
                         line_list.append(element)
+                        line_list1.append(crypto(key, BBW,rsi))
+                        for i in line_list1:
+                            print()
         except (TypeError):
             k = 1
         except (ZeroDivisionError):
             k=0
-    return render_template('data.html', line_list=line_list, hours=hours)
+    return render_template('data.html', line_list=line_list, hours=hours, line_list1=line_list1 )
 @app.errorhandler(404)
 def pageNotFound(error):
     return render_template('error.html')
+
