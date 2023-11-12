e2cedde24a4ec78a820bb199c6e61e6fb4fbab4a
atilaahmettaner <atilaahmet07@gmail.com>
2023-11-13 01:24:51 +0300

    bug fixed

--- app.py
+++ app.py
@@ -143,4 +143,4 @@ def pageNotFound(error):
     return render_template('error.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5001)))
